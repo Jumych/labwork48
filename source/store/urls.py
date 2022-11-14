d8f97183bc33d2809daab041b33463b0543c5b38
Malikov_Jumadil <maleevish@gmail.com>
2022-11-14 18:27:43 +0600

    Добавил Главную страницу, для просмотра Продуктов

--- source/store/urls.py
+++ source/store/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import index_view, eshop_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index_view, name='index'),
+    path('eshop/<int:pk>/', eshop_view, name='eshop_view')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from webapp.models import Product
 
+
 # Create your views here.
 def index_view(request):
     eshop = Product.objects.order_by
@@ -9,3 +10,8 @@ def index_view(request):
         'eshop': eshop
     }
     return render(request, 'index.html', context)
+
+
+def eshop_view(request, pk):
+    eshop = get_object_or_404(Product, pk=pk)
+    return render(request, 'eshop_view.html', {'eshop': eshop})
